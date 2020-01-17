bc56484c83466c68131c1fcb7ffa32d65a2a9414
Eduard Castro <eduard.cas07@gmail.com>
2020-01-16 21:05:43 -0500

    Add spring boot with thymeleaf - conditions

---
+++ thymeleaf/src/main/java/com/example/thymeleaf/ConditionController.java
@@ -0,0 +1,33 @@
+package com.example.thymeleaf;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ConditionController {
+
+    @GetMapping("/condition")
+    public String condition(Model model) {
+        double grade = 90.5;
+        model.addAttribute("grade", grade);
+        model.addAttribute("GPA", convertGPA(grade));
+
+        return "condition";
+    }
+
+    private String convertGPA(double grade) {
+        if (grade >= 90) {
+            return "A";
+        } else if (grade < 90 && grade >= 80) {
+            return "B";
+        } else if (grade < 80 && grade >= 70) {
+            return "C";
+        } else if (grade < 70 && grade >= 60) {
+            return "D";
+        } else {
+            return "F";
+        }
+    }
+
+}
